a0af98ac287443801f622f03b5613b542e8df8c5
starwarsgood <comp822012016@mail.ru>
2016-10-11 16:39:53 +0300

    gg

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'math.php';
+$object = new math;
+$object -> sum(2,3);
+echo $object -> lol;
+?>
---
+++ math.php
@@ -0,0 +1,10 @@
+<?php
+ class math{
+	 public $lo,$ol,$lol;
+	 function sum ($lo,$ol){
+		 $this-> $lo;
+		 $this-> $ol;
+		 $this-> lol=$ol + $lo;
+	 }
+ };
+?>
